96badfbf7a0e5e19f070e9ad8d2bed7ead449e0a
Danilo Pereira <danilo.ppessoa2610@gmail.com>
2024-04-23 15:19:35 -0300

    Feat: Criando o repositório

---
+++ src/main/java/com/danilopereira/my_school_transportation_v20/Application.java
@@ -0,0 +1,13 @@
+package com.danilopereira.my_school_transportation_v20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilopereira/my_school_transportation_v20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilopereira.my_school_transportation_v20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
