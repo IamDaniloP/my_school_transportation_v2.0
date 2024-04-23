eecc56146bb450bd87447a030aedd9a0b17dc0d0
Danilo Pereira <danilo.ppessoa2610@gmail.com>
2024-04-23 16:51:50 -0300

    Feat: Criando enum Months

---
+++ src/main/java/com/danilopereira/my_school_transportation_v20/domain/enums/Months.java
@@ -0,0 +1,5 @@
+package com.danilopereira.my_school_transportation_v20.domain.enums;
+
+public enum Months {
+    JANEIRO, FEVEREIRO, MARCO, ABRIL, MAIO, JUNHO, JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+}
