02754a049b453982cc584e6d73ba1eb726e50b90
Danilo Pereira <danilo.ppessoa2610@gmail.com>
2024-04-30 15:16:31 -0300

    Feat: Criando exception InvalidShift

---
+++ src/main/java/com/danilopereira/my_school_transportation_v20/domain/exceptions/InvalidShiftException.java
@@ -0,0 +1,7 @@
+package com.danilopereira.my_school_transportation_v20.domain.exceptions;
+
+public class InvalidShiftException extends RuntimeException {
+    public InvalidShiftException() {
+        super("Invalid Shift Exception");
+    }
+}
